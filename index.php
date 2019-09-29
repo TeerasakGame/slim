d0f483c6ae263f57340e2fa6bd879b8ef47ce424
Teerasak <manu98765@hotmail.com>
2019-09-30 01:06:31 +0700

    add status

--- index.php
+++ index.php
@@ -10,14 +10,18 @@ $app->get('/', function ($request, $response, $args) {
 //get all
 $app->get('/user', function ($request,  $response, $args) {
     include 'database.php';
-    $sql = "SELECT id, name FROM user";
+    $sql = "SELECT id, name FROM user  order by id";
     $result = $conn->query($sql);
+    $data = array();
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            echo json_encode($row);
+            array_push($data,$row);
         }
+        $payload = json_encode($data);
+        $response->getBody()->write($payload);
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
     } else {
-        echo json_encode(false);
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(204);
     }
 });
 // get by id
@@ -25,24 +29,39 @@ $app->get('/user/{id}', function ($request,  $response, $args) {
     include 'database.php';
     $sql = "SELECT id, name FROM user WHERE id = ".$args['id'];
     $result = $conn->query($sql);
+    $data = array();
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            echo json_encode($row);
+            array_push($data, $row);
         }
+        $payload = json_encode($data);
+        $response->getBody()->write($payload);
+        return $response->withStatus(200);
     } else {
-        echo json_encode(false);
+        return $response->withStatus(204);
     }
 });
 //add user
 $app->post('/user', function ($request,  $response, $args) {
     include 'database.php';
     $name = $request->getparam('name');
-    $sql = "INSERT INTO user (name) VALUES ('$name');";
-    if ($conn->query($sql) === TRUE) {
-        echo json_encode(true);;
-    } else {
-        echo json_encode(false);
+    if($name){
+        $sql = "INSERT INTO user (name) VALUES ('$name');";
+        if ($conn->query($sql) === TRUE) {
+            $payload = json_encode(true);
+            $response->getBody()->write($payload);
+            return $response->withStatus(201);
+        } else {
+            $payload = json_encode(false);
+            $response->getBody()->write($payload);
+            return $response->withStatus(200);
+        }
+    }else{
+        $payload = json_encode(false);
+        $response->getBody()->write($payload);
+        return $response->withStatus(200);
     }
+    
 });
 //update
 $app->put('/user/{id}', function ($request,  $response, $args) {
@@ -51,9 +70,15 @@ $app->put('/user/{id}', function ($request,  $response, $args) {
     $id = $args['id'];
     $sql = "UPDATE user SET name='$name' WHERE id= $id";
     if ($conn->query($sql) === TRUE) {
-        echo json_encode(true);;
+        // echo json_encode(true);
+        $payload = json_encode(true);
+        $response->getBody()->write($payload);
+        return $response->withStatus(200);
     } else {
-        echo json_encode(false);
+        // echo json_encode(false);
+        $payload = json_encode(false);
+        $response->getBody()->write($payload);
+        return $response->withStatus(200);
     }
 });
 //delete
@@ -62,9 +87,13 @@ $app->delete('/user/{id}', function ($request,  $response, $args) {
     $id = $args['id'];
     $sql = "DELETE FROM user WHERE id=$id";
     if ($conn->query($sql) === TRUE) {
-        echo json_encode(true);;
+        $payload = json_encode(true);
+        $response->getBody()->write($payload);
+        return $response->withStatus(200);
     } else {
-        echo json_encode(false);
+        $payload = json_encode(false);
+        $response->getBody()->write($payload);
+        return $response->withStatus(200);
     }
 });
 
